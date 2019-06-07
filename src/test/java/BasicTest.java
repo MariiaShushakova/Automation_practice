b5414fbbe942e70bf3dbd28ad4413bfe526d1843
Mariia_Shushakova <mariia.shushakova@gmail.com>
2019-06-07 16:20:08 +0300

    tests for seleniumeasy.com

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,9 +1,18 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 abstract public class BasicTest {
+    private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(BasicTest.class);
     protected WebDriver driver;
 
     @BeforeMethod
@@ -13,8 +22,20 @@ abstract public class BasicTest {
     }
 
     @AfterMethod
-    protected void closeBrowser() {
+    protected void closeBrowser(ITestResult testResult) {
+        makeScreenShot(testResult);
         driver.quit();
     }
 
+    private void makeScreenShot(ITestResult testResult) {
+        if (!testResult.isSuccess()) {
+            File scr = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+            try {
+                FileUtils.moveFile(scr, new File("d:\\scr\\" + testResult.getName() + ".png"));
+            } catch (IOException e) {
+                LOGGER.error(e.getMessage());
+            }
+        }
+    }
 }
--- src/test/java/CheckBoxTest.java
+++ src/test/java/CheckBoxTest.java
@@ -1,17 +1,18 @@
+import org.apache.log4j.Logger;
 import org.assertj.core.api.Assertions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import pages.CheckBoxPage;
 
 
 public class CheckBoxTest extends BasicTest {
+    private static final Logger LOGGER = Logger.getLogger(CheckBoxTest.class);
+
     @Test
     public void singleCheckboxTest(){
+        LOGGER.info("invoke single CheckBoxTest");
         String outputMesage = new CheckBoxPage(driver).singleCheckBoxPage();
 
+        LOGGER.error("Error!");
         Assertions.assertThat("Success - Check box is checked").isEqualTo(outputMesage);
     }
 }
